d80678713c3205a315fdafb6e5a09ab52990858e
Ron David <rond@automationage.com>
2016-11-11 13:44:02 +0200

    first upload

---
+++ SampleProj/src/test/java/sample/Sample_tests.java
@@ -0,0 +1,87 @@
+package sample;
+
+
+import junit.framework.Assert;
+import org.junit.*;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+
+
+/**
+ * Created by rondlap on 11/11/2016.
+ */
+public class Sample_tests {
+
+    //====================================================================================
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("Before test");
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("After test");
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @Test
+    public void pass1_test() throws Exception {
+        System.out.println("Test that passes");
+        Assert.assertTrue(true);
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @Test
+    public void pass2_test() throws Exception {
+        System.out.println("Test that passes");
+        Assert.assertTrue(true);
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @Test
+    public void fail_test() throws Exception {
+        System.out.println("Test that fails");
+        Assert.assertTrue(false);
+    }
+    //====================================================================================
+
+    @BeforeClass
+    //====================================================================================
+    public static void classSetup(){
+    }
+    //====================================================================================
+
+    @AfterClass
+    //====================================================================================
+    public static void classCleanup(){
+
+    }
+    //====================================================================================
+
+    @Rule
+    //====================================================================================
+    /**
+     * this method sets sets some parameters and prints to log
+     */
+    public TestRule watcher = new TestWatcher() {
+        protected void starting(Description description) {
+
+        }
+        protected void failed(Throwable e, Description description) {
+
+        }
+        protected void succeeded (Description description) {
+        }
+        protected void finished (Description description ){
+
+        }
+    };
+    //====================================================================================
+}
