ce78fccfff87e0177c130ae1d81b1118790d5d08
ron296david <rond@automationage.com>
2016-11-19 19:55:16 +0200

    change

--- SampleProj/src/test/java/sample/Sample_Test.java
+++ SampleProj/src/test/java/sample/Sample_Test.java
@@ -30,7 +30,7 @@ public class Sample_Test {
 
     //====================================================================================
     @Test
-    public void pass1_test() throws Exception {
+    public void pass1_Test() throws Exception {
         System.out.println("Test that passes");
         Assert.assertTrue(false);
     }
@@ -38,66 +38,10 @@ public class Sample_Test {
 
     //====================================================================================
     @Test
-    public void pass2_test() throws Exception {
+    public void pass2_Test() throws Exception {
         System.out.println("Test that passes");
         Assert.assertTrue(true);
     }
     //====================================================================================
 
-    //====================================================================================
-    @Test
-    public void fail_test() throws Exception {
-        System.out.println("Test that fails 1");
-        Assert.assertTrue(true);
-    }
-    //====================================================================================
-
-    //====================================================================================
-    @Test
-    public void fail3_test() throws Exception {
-        System.out.println("Test that fails 1");
-        Assert.assertTrue(true);
-    }
-    //====================================================================================
-
-    //====================================================================================
-    @Test
-    public void isInMaster_test() throws Exception {
-        System.out.println("look for it in maste   r");
-        Assert.assertTrue(true);
-    }
-    //====================================================================================
-
-    @BeforeClass
-    //====================================================================================
-    public static void classSetup(){
-    }
-    //====================================================================================
-
-    @AfterClass
-    //====================================================================================
-    public static void classCleanup(){
-
-    }
-    //====================================================================================
-
-    @Rule
-    //====================================================================================
-    /**
-     * this method sets sets some parameters and prints to log
-     */
-    public TestRule watcher = new TestWatcher() {
-        protected void starting(Description description) {
-
-        }
-        protected void failed(Throwable e, Description description) {
-
-        }
-        protected void succeeded (Description description) {
-        }
-        protected void finished (Description description ){
-
-        }
-    };
-    //====================================================================================
 }
