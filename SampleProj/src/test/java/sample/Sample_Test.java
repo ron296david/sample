358d0f3f87829c60fcdd7b6acb8e1f8c77e36058
ron296david <rond@automationage.com>
2016-11-19 19:47:02 +0200

    changed file name

---
+++ SampleProj/src/test/java/sample/Sample_Test.java
@@ -0,0 +1,103 @@
+package sample;
+
+
+import junit.framework.Assert;
+import org.junit.*;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+
+
+/**
+ * Created by rondlap on 11/11/2016.
+ */
+public class Sample_Test {
+
+    //====================================================================================
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("Before testdsfdsf ");
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("After test");
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @Test
+    public void pass1_test() throws Exception {
+        System.out.println("Test that passes");
+        Assert.assertTrue(true);
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @Test
+    public void pass2_test() throws Exception {
+        System.out.println("Test that passes");
+        Assert.assertTrue(true);
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @Test
+    public void fail_test() throws Exception {
+        System.out.println("Test that fails 1");
+        Assert.assertTrue(true);
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @Test
+    public void fail3_test() throws Exception {
+        System.out.println("Test that fails 1");
+        Assert.assertTrue(true);
+    }
+    //====================================================================================
+
+    //====================================================================================
+    @Test
+    public void isInMaster_test() throws Exception {
+        System.out.println("look for it in maste   r");
+        Assert.assertTrue(true);
+    }
+    //====================================================================================
+
+    @BeforeClass
+    //====================================================================================
+    public static void classSetup(){
+    }
+    //====================================================================================
+
+    @AfterClass
+    //====================================================================================
+    public static void classCleanup(){
+
+    }
+    //====================================================================================
+
+    @Rule
+    //====================================================================================
+    /**
+     * this method sets sets some parameters and prints to log
+     */
+    public TestRule watcher = new TestWatcher() {
+        protected void starting(Description description) {
+
+        }
+        protected void failed(Throwable e, Description description) {
+
+        }
+        protected void succeeded (Description description) {
+        }
+        protected void finished (Description description ){
+
+        }
+    };
+    //====================================================================================
+}
