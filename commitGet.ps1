4e04a1ca0198df0fe53e9809aee7933f59f79685
ron296david <rond@automationage.com>
2016-11-20 19:12:33 +0200

    added ps file

---
+++ commitGet.ps1
@@ -0,0 +1,9 @@
+function Get-CommitsFromGitLog([string] $StartCommit, [string] $EndCommit){
+    $Cmd = "git log --pretty=format:""CommitHash=%H :: AuthorEmail=%ae :: AuthorDate=%ad :: Subject=%s"" $StartCommit...$EndCommit"
+    #$Cmd = "git log Subject=%s"" $StartCommit...$EndCommit"
+ 
+    $Result = Invoke-Expression $Cmd
+    $ParsedResult = $Result | %{ ConvertFrom-StringData($_ -replace " :: ", "`n")  }
+ 
+    return $ParsedResult
+}
