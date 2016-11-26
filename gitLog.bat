1c7092b698c5dab49925bf83a8e85cf99d89f382
ron296david <rond@automationage.com>
2016-11-26 11:37:44 +0200

    branchFinished - committed

--- gitLog.bat
+++ gitLog.bat
@@ -1,11 +1,20 @@
 @echo off
-
+set BRANCHFINISH=%1
+echo Look for: %BRANCHFINISH%
 for /f %%i in ('git log -1') do set VAR=%%i
 rem git log -1>temp.txt
 rem set /p VAR=<temp.txt
-echo %VAR%
+echo Git last comment  = %VAR%
 
 
+if %VAR% == %BRANCHFINISH% (
+	echo Branch is finished - merging and trying to push to master...
+	echo "Merge and push to master"
+	git checkout master
+	git pull origin master
+	git merge masterCopy
+	git push origin master
+)
 
 rem if "finish" in %VAR% goto FoundIt 
 rem echo Did not find finish.
