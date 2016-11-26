0eb3947ec358cc8fb32f199d973f7ce4ed76f446
ron296david <rond@automationage.com>
2016-11-26 11:24:48 +0200

    branchFinished - message 2

--- gitLog.bat
+++ gitLog.bat
@@ -1,11 +1,12 @@
 @echo off
+set FINISHBRANCH=%1
 
 for /f %%i in ('git log -1') do set VAR=%%i
 rem git log -1>temp.txt
 rem set /p VAR=<temp.txt
 echo %VAR%
 
-
+if %VAR% == %FINISHBRANCH% echo pushing to master
 
 rem if "finish" in %VAR% goto FoundIt 
 rem echo Did not find finish.
